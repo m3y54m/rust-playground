b307bc121dd5673a325df15998aa47d5408f2adc
Meysam Parvizi <meysam.parvizi.ir@gmail.com>
2023-02-05 18:40:54 +0330

    Programming a Guessing Game

--- README.md
+++ README.md
@@ -1,9 +1,37 @@
 # Rust Playground
 
+## Build and Run
+
+### Using `rustc` (Not recommended):
+
+```
+rustc src/main.rs
+./main 
+```
+
+### Using `cargo`:
+
+Build without running:
+
+```
+cargo build
+```
+
+Build and run:
+
+```
+cargo run
+```
+
+Only check the source code:
+
+```
+cargo check
+```
+
 ## Resources
 
 - Learning step-by-step from [the book!](https://doc.rust-lang.org/book/title-page.html)
 - [Rust by Example](https://doc.rust-lang.org/rust-by-example/)
-
-
-
+- [Rust Cookbook](https://rust-lang-nursery.github.io/rust-cookbook/)
+- [A Gentle Introduction To Rust](https://stevedonovan.github.io/rust-gentle-intro/)
--- src/main.rs
+++ src/main.rs
@@ -1,3 +1,39 @@
+use rand::Rng;
+use std::cmp::Ordering;
+use std::io;
+
 fn main() {
-    println!("Hello World!");
+    println!("Guess the number!");
+
+    // Generate a random number between 1~100 (including 100)
+    let secret_number = rand::thread_rng().gen_range(1..=100);
+
+    println!("The secret number is {secret_number}");
+
+    loop {
+        println!("Please input your guess:");
+
+        // Create an empty string
+        let mut guess = String::new();
+
+        // Get string from user input
+        io::stdin()
+            .read_line(&mut guess)
+            .expect("Failed to read line");
+
+        // Convert string to number
+        let guess: u32 = guess.trim().parse().expect("Please type a number!");
+
+        println!("You guessed: {guess}");
+
+        // Compare guess with secret_number
+        match guess.cmp(&secret_number) {
+            Ordering::Less => println!("Too small!"),
+            Ordering::Greater => println!("Too big!"),
+            Ordering::Equal => {
+                println!("You win!");
+                break;
+            }
+        }
+    }
 }
